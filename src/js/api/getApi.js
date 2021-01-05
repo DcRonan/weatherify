b51de4609376cfa7a2d63af3739d3da0a09b8742
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-05 09:35:53 +0000

    edit getWeatherData func

--- src/js/api/getApi.js
+++ src/js/api/getApi.js
@@ -1,6 +1,6 @@
 const getWeatherData = async city => {
   const response = await fetch(
-    `api.openweathermap.org/data/2.5/weather?q=${city}&appid=b65d7b54d34b0aecf82c86c39aabf664`,
+    `api.openweathermap.org/data/2.5/weather?q=${city}&appid=b65d7b54d34b0aecf82c86c39aabf664&units=metric`,
   );
   const data = await response.json();
   return data;
