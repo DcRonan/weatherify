b7c31f6e10fc39e39f7639367152f44535b61a32
Daniel Ronan <61019884+DcRonan@users.noreply.github.com>
2021-01-05 08:43:02 +0000

    Initialize tailwind-css

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+          'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
